a495f272d2d9a3d077a082b9906c12d4ce7df97c
alexedwards15 <alexedwards1@u.boisestate.edu>
2016-10-26 11:59:54 -0600

    Homework and Lecture

--- Lecture/L17/practical_17.m
+++ Lecture/L17/practical_17.m
@@ -19,4 +19,20 @@ Q = trapz( x, y ); % trapz(x-vector, y-vector)
 f = @(x) sin(x);
 Qi = integral( f, 0, pi, 'AbsTol', 1.e-8 ); % new method
 
+%% Class Exercise
+f = @(y) sqrt(y+1)
+Q = quad( f, 0, 3 )
+
+%%
+f = @(r) ((5.*r)./((4+(r.^2)).^2))
+R = integral( f, -1, 1)
+
+%% 
+f = @(t) ((cos(2.*t)).^3)*sin(2.*t)
+b = pi/6
+t = integral( f, 0, b, 'AbsTol' 1.e-8 )
+%% 
+x = linspace(0,5,101);
+y = x.*exp(-x)
+plot(x,y,'b')
 
